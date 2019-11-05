c59b06295db0ba935eca8ca2d7376fc32e7f83e0
Raja Khan <getrk@yahoo.com>
2019-11-05 18:09:23 -0500

    Create lambda_function1.py

---
+++ src/lambda_function1.py
@@ -0,0 +1,9 @@
+from __future__ import print_function
+
+import json
+
+print('Loading function - Task-2')
+
+def lambda_handler(event, context):
+    print ("TASK-2 Executed")
+    return "Task-2 Lambda"
